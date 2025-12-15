a452d7f99f9d50107edc8fc212c3f0380adb531d
CyberDruga <soares.henrique495@gmail.com>
2025-12-15 07:15:33 -0300

    Changing extensions repo

--- flake.nix
+++ flake.nix
@@ -39,8 +39,8 @@
 
         overlay = final: prev: {
 
-          gnomeExtensions = unstable.gnomeExtensions //  {
-            window-is-ready-remover = unstable.gnomeExtensions.window-is-ready-remover.overrideAttrs {
+          gnomeExtensions = prev.gnomeExtensions //  {
+            window-is-ready-remover = prev.gnomeExtensions.window-is-ready-remover.overrideAttrs {
               postInstall = /*bash*/ ''
              cd $out/share/gnome-shell/extensions/windowIsReady_Remover@nunofarruca@gmail.com
              ${unstable.nushell}/bin/nu -c "open metadata.json | update shell-version { \$in ++  [ \"48\"] } | save metadata.json --force "
